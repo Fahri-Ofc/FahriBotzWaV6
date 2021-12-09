1aea76a634f639d131e5bf156376891de16130fa
BOTZ4YOU <fahrigaannzz@gmail.com>
2021-12-09 17:33:50 +0700

    FahriBotzWaV6

---
+++ shop/allpayment.js
@@ -0,0 +1,38 @@
+const allpayment = (prefix) => {
+return `PAYMENT BY FAHRI BOTZ
+
+*🏧 BANK*
+
+1. BRI 
+	ㅁ 8881 0857 7267 4695
+2. BCA 
+	ㅁ 3901 0857 7267 4695
+3. BNI 
+	ㅁ 881 0857 7267 4695
+4. PERMATA
+	ㅁ 852 0857 7267 4695
+	
+*💰 E-MONEY*
+
+1. GOPAY
+	ㅁ Lewat Qriss Aja Kak
+2. OVO
+	ㅁ 0857 7267 4695
+3. DANA
+	ㅁ 0857 7267 4695
+	
+Semuanya atas nama F****S****
+
+*📲 PULSA*
+
+1. IM3/INDOSAT (+5K)
+	ㅁ 0857 7267 4695
+2. SMARTFREN (+5K)
+	ㅁ 0889 0570 3349
+	
+Sebelum melakukan pembayaran ada baiknya anda menghubungi owner terlebih dahulu!
+`
+	}
+
+exports.allpayment = allpayment
+ 
---
+++ shop/donasi.js
@@ -0,0 +1,29 @@
+const donasibot = () => {
+return `DONASI BISA VIA BANK / E-MONEY
+
+*🏧 BANK*
+
+1. BRI 
+	ㅁ 8881 0857 7267 4695
+2. BCA 
+	ㅁ 3901 0857 7267 4695
+3. BNI 
+	ㅁ 881 0857 7267 4695
+4. PERMATA
+	ㅁ 852 0857 7267 4695
+	
+*💰 E-MONEY*
+
+1. GOPAY
+	ㅁ Qriss Aja Kak
+2. OVO
+	ㅁ 0857 7267 4695
+3. DANA
+	ㅁ 0857 7267 4695
+	
+Semuanya atas nama F****S****
+`
+	}
+
+exports.donasibot = donasibot
+ 
